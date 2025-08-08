b0ddb33a0cfff03786c15fea019f143942fd82cf
SHEKHAR2642 <shekhar2642000@gmail.com>
2025-08-08 19:03:21 +0530

    Fixed Expenses entity with @Id annotation and updated MySQL config

--- project/src/main/java/com/expense/project/entity/Expenses.java
+++ project/src/main/java/com/expense/project/entity/Expenses.java
@@ -1,2 +1,23 @@
-package com.expense.project.entity;public class Expenses {
+package com.expense.project.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="expenses")
+public class Expenses {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto increment in MySQL
+    private Long id;
+    @Column
+    private String title;
+    @Column
+    private String category;
+    @Column
+    private double amount;
+    @Column
+    private LocalDate date;
+    @Column
+    private String notes;
 }
